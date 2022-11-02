368adbebf55891ded35c9c13cea78c386c9c8541
NguyenTruong150197 <truong12121996@gmail.com>
2022-11-02 19:03:04 +0700

    second commit

--- t2207e-nodejs/index.js
+++ t2207e-nodejs/index.js
@@ -5,85 +5,118 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
     console.log("Server is running...");
 });
+// config to connect mysql
+const configDB = {
+    host: "139.180.186.20",
+    port: 3306,
+    database: "t2207e",
+    user: "t2207e",
+    password: "t2207e123", // mamp : "root" -- xampp: "" (ai dùng mamp thì điền là "root", còn dùng xampp thì để "")
+    multipleStatements: true // cho phép sử dụng nhiều câu SQL 1 lần gửi yêu cầu
+}
 
-var ls = [
-    {
-        id: 1,
-        name: "Nguyễn Hoàng Nam",
-        age: 18
-    },
-    {
-        id: 2,
-        name: "Thái Sơn",
-        age: 16
-    }
-];
+// connect to mysql
+const mysql = require("mysql");
+const conn = mysql.createConnection(configDB);
 
-app.get("/demo", function (req, res) {
-    res.send("Hello World!");
-});
-
-app.get("/get-data", function (req, res) {
-    res.send(ls);
-});
-
-app.get("/detail", function (req, res) {
-    var paramID = req.query.id;
-    var data;
-    for(var i = 0; i < ls.length; i++){
-        if(ls[i].id == paramID){
-            data = ls[i];
-            break;
+// api list all class
+app.get("/get-classes", function (req, res){
+   const sql = "select * from classes";
+   conn.query(sql, function (err, data){
+        if(err){
+            res.send("404 not found");
         }
-    }
-    res.send(data);
+        else{
+            res.send(data);
+        }
+   })
 });
 
-app.get("/edit", function (req, res) {
-    var paramID = req.query.id;
-    var paramName = req.query.name;
-    var paramAge = req.query.age;
-    for(var i = 0; i < ls.length; i++){
-        if(ls[i].id == paramID){
-            ls[i].name = paramName;
-            ls[i].age = paramAge;
-            break;
+// api list all student
+app.get("/get-students", function (req, res){
+    const sql = "select * from students";
+    conn.query(sql, function (err, data){
+        if(err){
+            res.send("404 not found");
         }
-    }
-    res.send("Done");
+        else{
+            res.send(data);
+        }
+    })
 });
 
-app.get("/add", function (req, res) {
-    var paramID = req.query.id;
-    var paramName = req.query.name;
-    var paramAge = req.query.age;
-    var check = false;
-    for (var i = 0; i < ls.length; i++){
-        if (ls[i].id == paramID){
-            check == true;
-            break;
+// lọc theo id
+app.get("/student-by-class", function (req, res){
+    const cid = req.query.id;
+    const sql = "select * from students where id = " + cid;
+    conn.query(sql, function (err, data){
+        if(err){
+            res.send("404 not found");
         }
-    }
-    if (check == false){
-        ls.push({
-            id: paramID,
-            name: paramName,
-            age: paramAge
-        });
-    }
-    res.send("Done");
+        else{
+            res.send(data);
+        }
+    })
 });
 
-app.get("/delete", function (req, res) {
-    var paramID = req.query.id;
-    var p = -1;
-    for (var i = 0; i < ls.length; i++) {
-        if (ls[i].id == paramID) {
-            p = i;
+// search student by name
+app.get("/search-student-by-name", function (req, res){
+    const name = req.query.name;
+    const sql = `select * from students where name like '%${name}%' or email like '%${name}%'`;
+    conn.query(sql, function (err, data){
+        if(err){
+            res.send("404 not found");
         }
-    }
-    if (p != -1){
-        ls.splice(p, 1);
-    }
-    res.send("Done");
+        else{
+            res.send(data);
+        }
+    })
+});
+
+// search students by class name
+app.get("/search-students-by-class-name", function (req, res){
+    const classes = req.query.n;
+    const sql = `select * from students where id in (select id from classes where name like '%${classes}%')`;
+    conn.query(sql, function (err, data){
+        if(err){
+            res.send("404 not found");
+        }
+        else{
+            res.send(data);
+        }
+    })
+});
+
+// get 1 student by id
+app.get("/detail-student", function (req, res){
+    const sid = req.query.id;
+    const sql = `select * from students where sid = ${sid}`;
+    conn.query(sql, function (err, data){
+        if(err){
+            res.status(403).send("404 not found");
+        }
+        else if(data.length > 0){
+            res.send(data[0]);
+        }
+        else {
+            res.status(404).send("404 not found");
+        }
+    })
+});
+
+app.get("/student", function (req, res) {
+    // liệt kê sinh viên
+    res.send("Student with GET");
+});
+app.post("/student", function (req, res) {
+    // thêm 1 sinh viên
+    res.send("Student with POST");
+});
+app.put("/student", function (req, res) {
+    // update sinh viên
+    res.send("Student with PUT");
+});
+app.delete("/student", function (req, res) {
+    // delete sinh viên
+    res.send("Student with DELETE");
 });
